d0f84db4c0d377f0b633783994290b6023a1bb96
Ryan Whitney <whitney.ryan.a@gmail.com>
2022-02-21 16:45:59 -0800

    all but two passing!!!!!!! YEET!

--- lib/author.rb
+++ lib/author.rb
@@ -12,21 +12,24 @@ class Author
         post.author = self
     end
 
-    def add_post_by_title(post)
-
+    def add_post_by_title(post_title)
+        new_post = Post.new(post_title)
+        new_post.author = self
+        new_post
     end
 
     def posts
-        # binding.pry
-        @@posts.select  { |post|
+        @@posts = Post.all
+        @@posts.select {|post|
             post.author == self
         }
     end
 
-    def post_count
-
+    def self.post_count
+        @@posts = Post.all
+        @@posts.length
     end
-
+    
 
 
 end
--- lib/post.rb
+++ lib/post.rb
@@ -2,17 +2,27 @@ require 'pry'
 
 class Post
 
-    attr_accessor :title
+    attr_accessor :title, :author
     @@all = []
     def initialize(title)
-
         @title = title
         @@all << self
     end
 
-    def author=(name)
-        Author.new(name)
+
+    def self.all
+        @@all
     end
+
+    def author_name
+       if self.author == nil
+        return nil
+       else 
+        return self.author.name
+       end
+    end
+
+
 
 
 end
